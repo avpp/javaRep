d6451a7c5f568cefc6ce375cd7e16391ee485501
Nuzhnykh Alexey <nuzhaleks2@rambler.ru>
2011-11-15 15:13:09 +0300

    first commit

---
+++ src/first/First.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first;
+
+/**
+ *
+ * @author Alexey
+ */
+public class First {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
