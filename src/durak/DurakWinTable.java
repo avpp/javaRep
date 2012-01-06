3a786751a9e6e306f5053ae81f3b8f7fc672c9bf
migal-drew <migal.drew@gmail.com>
2012-01-07 02:27:35 +0300

    Client dont work

---
+++ src/durak/DurakWinTable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package durak;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Andrew
+ */
+public class DurakWinTable {
+    private LinkedList<String> m_winners;
+    
+    public void addWinner(String name) {
+        m_winners.add(name);
+    }
+    
+    public void clearAll() {
+        m_winners.clear();
+    }
+}
